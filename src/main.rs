fd6d29af0a128781e9c8123fc8fdb64c2ca2cf29
Sean Andersen <seana@squareup.com>
2021-12-17 20:04:19 -0800

    calculate length with references and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 fn main() {
-    let s1 = gives_ownership();
-    println!("s1 is: {}", s1);
+    let s1 = String::from("hello");
 
-    let s2 = String::from("hello");
+    let len = calculate_length(&s1);
 
-    let s3 = takes_and_gives_back(s2);
-
-    println!("s3 is: {}", s3);
+    println!("The length of '{}' is {}.", s1, len);
 }
 
-fn gives_ownership() -> String {
-    let some_string = String::from("yours");
-    some_string
-}
-
-fn takes_and_gives_back(a_string: String) -> String {
-    a_string
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
