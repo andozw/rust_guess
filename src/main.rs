9b22a94aefc220b1d187265414dbc8f8701f53da
Sean Andersen <seana@squareup.com>
2021-12-19 20:31:33 -0800

    data with enum

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
-}
-
 
 fn route(ip_kind: IpAddrKind) {}
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 
-    route(four);
-    route(six);
-
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+    route(home);
+    route(loopback);
 }
