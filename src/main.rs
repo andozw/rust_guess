61d3110aa22aeca0e674955e2ef01dedc64c5cb3
Sean Andersen <seana@squareup.com>
2021-12-21 08:30:45 -0800

    use with relative path and aliasing

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,13 @@ mod front_of_house {
 
 use crate::front_of_house::hosting;
 
+// can also use relative paths with `self`
+// and bring in conflicting names by aliasing with `as`
+use self::front_of_house::hosting as ghosting;
+
 fn main() {
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
     hosting::add_to_waitlist();
+    ghosting::add_to_waitlist();
 }
