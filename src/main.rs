a318bc8dc65445f1a7003537c1c4b9a26bf3c5c5
Sean Andersen <seana@squareup.com>
2021-12-21 08:26:03 -0800

    bring in scope with `use`

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,13 @@
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-
-        pub fn get_fruit(&self) -> &String {
-            &self.seasonal_fruit
-        }
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
 }
 
+use crate::front_of_house::hosting;
 
-fn main() { 
-    let breakfast = back_of_house::Breakfast::summer("rye");
-
-    println!("Toast is: {}, fruit is: {}", breakfast.toast, breakfast.get_fruit());
+fn main() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 }
