91dcb8c528f1cf73c05979968f3fbf1e9690f1c1
Sean Andersen <seana@squareup.com>
2021-12-22 10:31:00 -0800

    Strings

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,20 @@
-enum SpreadsheetCell {
-    Int(i32),
-    Float(f64),
-    Text(String),
-}
-
 fn main() {
-    let v1: Vec<i32> = Vec::new();
-    let v2 = vec![1, 2, 3];
+    let mut s = String::new();
+    let data = "initial contents";
 
-    let third: &i32 = &v2[2];
-    println!("The third element is: {}", third);
+    let s = data.to_string();
 
-    match v2.get(2) {
-        Some(third) => println!("The matched element is: {}", third),
-        None => println!("No third element"),
-    }
+    let s = "initial contents".to_string();
 
-    let mut v3 = Vec::new();
+    let s = String::from("initial contents");
 
-    v3.push(9);
-    v3.push(8);
-    v3.push(7);
-    v3.push(6);
+    let mut s = String::from("foo");
+    s.push_str("bar");
 
-    for i in &v3 {
-        println!("[{}]", i);
-    }
-    
-    for i in &mut v3 {
-        *i *= 11;
-        println!("[{}]", i);
-    }
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
 
-    let row = vec![
-        SpreadsheetCell::Int(3),
-        SpreadsheetCell::Text(String::from("blue")),
-        SpreadsheetCell::Float(10.12),
-    ];
-
-    // get returns None when element does not exist.
-    let will_not_panic = v3.get(100);
-    let will_panic = &v3[100];
+    s1.push('X');
+    println!("s1: {}, s2:{}", s1, s2);
 }
