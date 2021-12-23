73f75cbe064bbf5a1c5341ea0e707740447c3290
Sean Andersen <seana@squareup.com>
2021-12-22 19:18:00 -0800

    long read from file, with error propagation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use std::fs::File;
+use std::io::{self, Read};
 
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+    
 fn main() {
-    let f = File::open("hello.txt").unwrap();
+    let username = read_username_from_file().unwrap();
 
-    // expect is like unwrap but you can set custom message.
-    let f = File::open("hello.txt").expect("Custom panic message");
+    println!("Username: {}", username);
 }
