ec152751a5d69cb00535804cbe364e1f07a2ac01
Sean Andersen <seana@squareup.com>
2021-12-22 19:32:37 -0800

    return Result from main to use ? sugar

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
+use std::error::Error;
 use std::fs::File;
-use std::io::Read;
 
-fn read_username_from_file() -> Result<String, std::io::Error> {
-    let mut s = String::new();
-    File::open("hello.txt")?.read_to_string(&mut s)?;
-    Ok(s)
-}
-    
-fn main() {
-    let username = read_username_from_file().unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let f = File::open("hello")?;
 
-    println!("Username: {}", username);
+    Ok(())
 }
