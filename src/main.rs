db47262c5fea4b1d08f0fd833df83f561577f30e
Sean Andersen <seana@squareup.com>
2021-12-22 20:36:03 -0800

    traits. money printer go brrr

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
-struct Point<T, U> {
-    x: T, 
-    y: U,
+pub trait Summary {
+    fn summarize(&self) -> String;
 }
 
-impl<T, U> Point<T, U> {
-    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-        Point {
-            x: self.x,
-            y: other.y
-        }
-    }
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
 
-    fn x(&self) -> &T {
-        &self.x
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self. headline, self.author, self.location)
     }
-    fn y(&self) -> &U {
-        &self.y
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
     }
 }
 
 fn main() {
-    let p1 = Point { x: 5, y: 10.4 };
-    let p2 = Point { x: "Zane", y: 'x' };
-    let p3 = p1.mixup(p2);
-    println!("p3: [{}, {}]", p3.x(), p3.y());
+    let tweet = Tweet {
+        username: String::from("jerome_p"),
+        content: String::from("Money printer go BRRRR"),
+        reply: false,
+        retweet: false,
+    };
 
-    // can just access directly
-    println!("p3: [{}, {}]", p3.x, p3.y);
+    println!("1 new tweet: {}", tweet.summarize());
 }
