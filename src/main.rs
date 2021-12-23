85ec41a259047af55cc0800d11df419cb98bee33
Sean Andersen <seana@squareup.com>
2021-12-22 19:47:33 -0800

    basically nothing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use std::error::Error;
-use std::fs::File;
+pub struct Guess {
+    value: i32
+}
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let f = File::open("hello")?;
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("guess must be between 1 and 100, got {}", value);
+        }
 
-    Ok(())
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+fn main() {
+    let g = Guess::new(77);
+    println!("Guess is {}", g.value());
 }
