bdaea6b8b5e0da9f4f3657fd8317bf0ca8439c05
Sean Andersen <seana@squareup.com>
2021-12-24 18:36:31 -0800

    lifetime annotation

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,20 @@
-pub trait Display {}
-
-// Trait Bound Syntax
-pub fn notify<T: Summary>(item: &T) {
-    println!("Brokest news! {}", item.summarize());
-}
-
-// syntactic sugar for notify
-pub fn notify_sugar(item: &impl Summary) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-// Can use different classes that implement the trait in the same method
-pub fn notify_mixed(item1: &impl Summary, item2: &impl Summary) {}
-
-// To force both to use the same type, use trait bound syntax
-pub fn notify_same<T: Summary>(item1: &T, item2: &T) {}
-
-// Specify multiple trait bounds with sugar
-pub fn motify(item: &(impl Summary + Display)) {}
-
-// And with trait bound syntax
-pub fn lotify<T: Summary + Display>(item: &T) {}
-
-// And an alternative way. 
-fn some_function<T: Display + Clone, U: Clone + std::fmt::Debug>(t: &T, u: &U) {}
-fn other_way<T, U>(t: &T, u: &U)
-    where T: Display + Clone, 
-    U: Clone + std::fmt::Debug
-{}
-
-// Returning types that implement traits
-fn returns_summarizable() -> impl Summary {
-    Tweet {
-        username: String::from("satoshi"),
-        content: String::from(""),
-        reply: false,
-        retweet: false,
-    }
-}
-
-pub trait Summary {
-    fn summarize_author(&self) -> String;
-
-    //fn summarize(&self) -> String;
-    
-    // default implementation.
-    fn summarize(&self) -> String {
-         format!("(Read more from {}...)", self.summarize_author())
-    }
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-impl Summary for NewsArticle {
-    //fn summarize(&self) -> String {
-        //format!("{}, by {} ({})", self. headline, self.author, self.location)
-    //}
-
-    // use default implementation of summarize.
-    
-    // Implement summarize_author
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.author)
-    }
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
+// When returning a reference from a function, the lifetime parameter for the 
+// return type needs to match the lifetime parameter for one of the parameters.
+// If the reference returned does not refer to one of the parameters, it must refer
+// to a value created within the function, which would be a dangling reference.
+fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.len() >= b.len() {
+        a
+    } else {
+        b
     }
 }
 
 fn main() {
-    let tweet = Tweet {
-        username: String::from("jerome_p"),
-        content: String::from("Money printer go BRRRR"),
-        reply: false,
-        retweet: false,
-    };
+    let string1 = String::from("longest");
+    let string2 = String::from("short");
 
-    let article = NewsArticle {
-        headline: String::from("Penguins win the Stanley Cup Champioship!"),
-        location: String::from("Pittsburge, PA, USA"),
-        author: String::from("Iceburgh"),
-        content: String::from(
-            "The Pittsburgh Penguins picked pottery paired pigs pumping pumpkins"),
-    };
+    let result = longest(string1.as_str(), string2.as_str());
 
-    println!("1 new tweet: {}", tweet.summarize());
-    println!("New article available! {}", article.summarize());
-
-    notify(&tweet);
-    notify_sugar(&tweet);
-    notify(&article);
-    notify_sugar(&article);
+    println!("Longest string is {}", result);
 }
