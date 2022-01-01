4477042bd8f54e61cab9793154f068dd745e067f
Sean Andersen <seana@squareup.com>
2021-12-31 18:13:33 -0800

    Closure example

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,68 @@
-use std::fmt::Display;
+use std::thread;
+use std::time::Duration;
 
-fn longest_with_announcement<'a, T>(
-    x: &'a str,
-    y: &'a str,
-    announcement: T
-) -> &'a str
+struct Cacher<T>
 where
-    T: Display,
+    T: Fn(u32) -> u32,
 {
-    println!("Announcement! {}", announcement);
+    calculation: T,
+    value: Option<u32>,
+}
 
-    if x.len() > y.len() {
-        x
+impl<T> Cacher<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!", 
+            expensive_result.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
     } else {
-        y
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!", 
+                expensive_result.value(intensity)
+            );
+        }
     }
 }
 
 fn main() {
-    let string1 = String::from("longest");
-    let string2 = String::from("short");
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
 
-    let result = longest_with_announcement(string1.as_str(), 
-        string2.as_str(),
-        "ANNOUNCEMENT");
-
-    println!("Longest string is {}", result);
+    generate_workout(simulated_user_specified_value, simulated_random_number);
 }
