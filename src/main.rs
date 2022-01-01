bb4b5f6bf0ee7cfd2a75dc84000a6d98b3afdcf4
Sean Andersen <seana@squareup.com>
2022-01-01 11:44:11 -0800

    Reference loop using RefCell

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,42 @@
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
 }
 
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
+}
 
-// Borrowing rules: 
-//      * you can have either (but not both of) one mutable reference or any number
-//        of immutable references.
-//      * references must always be valid.
-//
-// With Box<T> the borrowing rules' invariants are enforced at compile time
-// With RefCell<T> the invariants are enforced at runtime.
-// Both are only allowed in single thread programs.
-// Because RefCell<T> allows mutable borrows checked at runtime, 
-// you can mutate the value inside the RefCell<T> even when the
-// RefCell<T> is immutable.
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    println!("count after creating a: {}", Rc::strong_count(&a));
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
 
-    // Rc::clone for shallow copies and reference count updates.
-    let b = Cons(3, Rc::clone(&a));
-    println!("count after creating b: {}", Rc::strong_count(&a));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
 
-    {
-        let c = Cons(4, Rc::clone(&a));
-        println!("count after creating c: {}", Rc::strong_count(&a));
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
     }
 
-    println!("count after c dies: {}", Rc::strong_count(&a));
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // Uncomment the next line to see that we have a cycle;
+    // it will overflow the stack
+    println!("a next item = {:?}", a.tail());
 }
